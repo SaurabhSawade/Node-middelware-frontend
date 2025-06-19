90e6b9c1414fd217290659950582a4bbefa4dae0
SaurabhSawade <saurabhsawade6@gmail.com>
2025-06-19 20:02:06 +0530

    improve link

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://node-middleware-backend.onrender.com',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
